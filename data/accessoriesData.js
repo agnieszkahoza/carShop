89a4aac8663e9002dda8254f020133f15a860301
agnieszkahoza <agnieszka.hoza@gmail.com>
2025-06-10 18:00:59 +0200

    fix(data): put the list of cars and accessories in a separate file, separate data from logic

---
+++ data/accessoriesData.js
@@ -0,0 +1,12 @@
+const accessories = [
+  { id: 1, name: "Floor mats ", price: 80 },
+  { id: 2, name: "Roof rack", price: 400 },
+  { id: 3, name: "Seat covers", price: 300 },
+  { id: 4, name: "Tow hitch", price: 100 },
+  { id: 5, name: "Sunshades", price: 50 },
+  { id: 6, name: "Parking sensors", price: 1200 },
+  { id: 7, name: "Seat-back organizer", price: 1200 },
+  { id: 8, name: "Rear-view camera", price: 1200 },
+];
+
+export default accessories;
